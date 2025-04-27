1d325577a0dd45fe1ff3554f46a0d95581a71f58
Farhan <anak.upload90@gmail.com>
2025-04-27 20:36:21 +0800

    feat: create error response writer helper

--- pkg/helper/json.go
+++ pkg/helper/json.go
@@ -1,9 +1,15 @@
 package helper
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+
+	"github.com/farhansaleh/layanan_aptika_be/constants"
+	"github.com/farhansaleh/layanan_aptika_be/internal/domain"
+	"github.com/go-sql-driver/mysql"
 )
 
 func WriteResponseBody(w http.ResponseWriter, statusCode int, data any) {
@@ -11,7 +17,7 @@ func WriteResponseBody(w http.ResponseWriter, statusCode int, data any) {
 	w.WriteHeader(statusCode)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
-		log.Println("Error encode: ", err)
+		log.Println("ERROR ENCODE: ", err)
 		panic(err)
 	}
 }
@@ -19,7 +25,53 @@ func WriteResponseBody(w http.ResponseWriter, statusCode int, data any) {
 func ParseBody(r *http.Request, data any){
 	err := json.NewDecoder(r.Body).Decode(data)
 	if err != nil {
-		log.Println("Error decode: ", err)
+		log.Println("ERROR DECODE: ", err)
 		panic(err)
 	}
+}
+
+func WriteErrorResponse(w http.ResponseWriter, err error) {
+	// validation error check
+	if validationErr, ok := IsValidationError(err); ok {
+		WriteResponseBody(w, http.StatusBadRequest, domain.ErrorValidationResponse{
+			Message: validationErr.Error(),
+			Errors: validationErr.Errors,
+		})
+		return
+	}
+	
+	// mysql error check
+	if mysqlError, ok := err.(*mysql.MySQLError); ok {
+		if mysqlError.Number == 1062 {
+			WriteResponseBody(w, http.StatusConflict, domain.DefaultResponse{
+				Message: mysqlError.Message,
+			})
+			return
+		}
+		
+		WriteResponseBody(w, http.StatusBadRequest, domain.DefaultResponse{
+			Message: mysqlError.Message,
+		})
+		return
+	}
+
+	// auth error check
+	if authError, ok := err.(*AuthError); ok {
+		WriteResponseBody(w, http.StatusUnauthorized, domain.DefaultResponse{
+			Message: authError.Message,
+		})
+		return
+	}
+
+	// error no rows
+	if errors.Is(err, sql.ErrNoRows){
+		WriteResponseBody(w, http.StatusNotFound, domain.DefaultResponse{
+			Message: constants.ErrorNotFound,
+		})
+		return
+	}
+
+	WriteResponseBody(w, http.StatusInternalServerError, domain.DefaultResponse{
+		Message: err.Error(),
+	})
 }
