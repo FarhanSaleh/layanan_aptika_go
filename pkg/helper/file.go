db6c7358e85f1df2b7759d6f9708c866f857bffd
Farhan <anak.upload90@gmail.com>
2025-05-02 11:36:40 +0800

    feat: create helper for handle upload file

---
+++ pkg/helper/file.go
@@ -0,0 +1,107 @@
+package helper
+
+import (
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func HandleUploadImage(w http.ResponseWriter, r *http.Request, fieldName string) (fileName string, err error) {
+	file, _, err := r.FormFile(fieldName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	uploadDir := filepath.Join(".", "uploads", "img")
+	if err = os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		return
+	}
+
+	mimeType, ext, err := GetMimeTypeFromMultipartForm(file)
+	if err != nil {
+		return
+	}
+
+	if mimeType != "image/png" && mimeType != "image/jpeg" && mimeType != "image/jpg" {
+		err = fmt.Errorf("unsupported file type: %s", ext)
+		return
+	}
+
+	uuid := uuid.NewString()
+	fileName = fmt.Sprintf("%s.%s", uuid, ext)
+	destPath := filepath.Join(uploadDir, fileName)
+	destFile, err := os.Create(destPath)
+	if err != nil {
+		return
+	}
+	defer destFile.Close()
+
+	if _, err = io.Copy(destFile, file); err != nil {
+		return
+	}
+
+	return
+}
+
+func HandleUploadPdf(w http.ResponseWriter, r *http.Request, fieldName string) (fileName string, err error) {
+	file, _, err := r.FormFile(fieldName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	uploadDir := filepath.Join(".", "uploads", "docs")
+	if err = os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		return
+	}
+
+	mimeType, ext, err := GetMimeTypeFromMultipartForm(file)
+	if err != nil {
+		return
+	}
+
+	if mimeType != "application/pdf" {
+		err = fmt.Errorf("unsupported file type: %s", ext)
+		return
+	}
+
+	uuid := uuid.NewString()
+	fileName = fmt.Sprintf("%s.%s", uuid, ext)
+	destPath := filepath.Join(uploadDir, fileName)
+	destFile, err := os.Create(destPath)
+	if err != nil {
+		return
+	}
+	defer destFile.Close()
+
+	if _, err = io.Copy(destFile, file); err != nil {
+		return
+	}
+
+	return
+}
+
+func GetMimeTypeFromMultipartForm(file multipart.File) (mimeType string, ext string, err error) {
+	buffer := make([]byte, 512)
+	if _, err = file.Read(buffer); err != nil {
+		return
+	}
+
+	mimeType = http.DetectContentType(buffer)
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return
+	}
+
+	ext = strings.Split(mimeType, "/")[1]
+
+	return
+}
